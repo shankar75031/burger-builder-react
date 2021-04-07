c3bec7a4cff597b4da691b3f16b5abc3a8c1407b
Prabhashankar Kannapan <shankar75031@gmail.com>
2021-04-08 00:28:23 +0530

    add backdrop and modal dismiss

--- src/containers/BurgerBuilder/BurgerBuilder.jsx
+++ src/containers/BurgerBuilder/BurgerBuilder.jsx
@@ -75,6 +75,12 @@ export default class BurgerBuilder extends Component {
     });
   };
 
+  purchaseCancelHandler = () => {
+    this.setState({
+      purchasing: false,
+    });
+  };
+
   render() {
     const disabledInfo = {
       ...this.state.ingredients,
@@ -86,7 +92,10 @@ export default class BurgerBuilder extends Component {
     }
     return (
       <React.Fragment>
-        <Modal show={this.state.purchasing}>
+        <Modal
+          show={this.state.purchasing}
+          modalClosed={this.purchaseCancelHandler}
+        >
           <OrderSummary ingredients={this.state.ingredients} />
         </Modal>
         <Burger ingredients={this.state.ingredients} />
