208438f5924326224d4df296fe6b4d7a21be1c9f
Prabhashankar Kannapan <shankar75031@gmail.com>
2021-04-08 02:17:23 +0530

    hide side drawer

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
 import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
 import classes from "./Layout.module.css";
 
-export default function Layout(props) {
-  return (
-    <React.Fragment>
-      <div>
-        <SideDrawer />
-        <Toolbar />
-        <main className={classes.Content}>{props.children}</main>
-      </div>
-    </React.Fragment>
-  );
+export default class Layout extends Component {
+  state = {
+    showSideDrawer: true,
+  };
+
+  sideDrawerCloseHandler = () => {
+    this.setState({ showSideDrawer: false });
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <div>
+          <SideDrawer
+            closed={this.sideDrawerCloseHandler}
+            open={this.state.showSideDrawer}
+          />
+          <Toolbar />
+          <main className={classes.Content}>{this.props.children}</main>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
--- src/components/Navigation/SideDrawer/SideDrawer.jsx
+++ src/components/Navigation/SideDrawer/SideDrawer.jsx
@@ -1,17 +1,25 @@
 import React from "react";
 import Logo from "../../Logo/Logo";
+import Backdrop from "../../UI/Backdrop/Backdrop";
 import NavigationItems from "../NavigationItems/NavigationItems";
 import classes from "./SideDrawer.module.css";
 
-export default function SideDrawer() {
+export default function SideDrawer(props) {
+  let attachedClasses = [classes.SideDrawer, classes.Close];
+  if (props.open) {
+    attachedClasses = [classes.SideDrawer, classes.Open];
+  }
   return (
-    <div className={classes.SideDrawer}>
-      <div className={classes.Logo}>
-        <Logo />
+    <React.Fragment>
+      <Backdrop show={props.open} clicked={props.closed} />
+      <div className={attachedClasses.join(" ")}>
+        <div className={classes.Logo}>
+          <Logo />
+        </div>
+        <nav>
+          <NavigationItems />
+        </nav>
       </div>
-      <nav>
-        <NavigationItems />
-      </nav>
-    </div>
+    </React.Fragment>
   );
 }
