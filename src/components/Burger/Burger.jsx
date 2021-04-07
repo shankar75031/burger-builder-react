c83039c511f07a39059e7132882d1dc21aac7752
Prabhashankar Kannapan <shankar75031@gmail.com>
2021-04-07 21:24:53 +0530

    added empty ingredients check

--- src/components/Burger/Burger.jsx
+++ src/components/Burger/Burger.jsx
@@ -3,15 +3,22 @@ import classes from "./Burger.module.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 export default function Burger(props) {
-  const transformedIngredients = Object.keys(props.ingredients).map(
-    (ingredientKey) => {
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map((ingredientKey) => {
       return [...Array(props.ingredients[ingredientKey])].map((_, index) => {
         return (
           <BurgerIngredient key={ingredientKey + index} type={ingredientKey} />
         );
       });
-    }
-  );
+    })
+    .reduce((arr, element) => {
+      return arr.concat(element);
+    }, []);
+  console.log(transformedIngredients);
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients!</p>;
+  }
+
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
