b544c117fe9b4389ce5e99c3e0a2180e027b1c4e
Prabhashankar Kannapan <shankar75031@gmail.com>
2021-04-07 21:14:31 +0530

    made burger component dynamic

--- src/components/Burger/Burger.jsx
+++ src/components/Burger/Burger.jsx
@@ -2,12 +2,20 @@ import React from "react";
 import classes from "./Burger.module.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
-export default function Burger() {
+export default function Burger(props) {
+  const transformedIngredients = Object.keys(props.ingredients).map(
+    (ingredientKey) => {
+      return [...Array(props.ingredients[ingredientKey])].map((_, index) => {
+        return (
+          <BurgerIngredient key={ingredientKey + index} type={ingredientKey} />
+        );
+      });
+    }
+  );
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      {transformedIngredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
