ddffc80d13dc283a622f3219a4c1e394f42c7f36
Prabhashankar Kannapan <shankar75031@gmail.com>
2021-04-08 02:37:16 +0530

    optimize modal

--- src/components/Burger/OrderSummary/OrderSummary.jsx
+++ src/components/Burger/OrderSummary/OrderSummary.jsx
@@ -1,30 +1,35 @@
-import React from "react";
+import React, { Component } from "react";
 import Button from "../../UI/Button/Button";
 
-export default function OrderSummary(props) {
-  const ingredientSummary = Object.keys(props.ingredients).map((igKey) => {
-    return (
-      <li key={igKey}>
-        <span style={{ textTransform: "capitalize" }}>{igKey}</span>:{" "}
-        {props.ingredients[igKey]}
-      </li>
+export default class OrderSummary extends Component {
+  render() {
+    console.log("RENDER CALL");
+    const ingredientSummary = Object.keys(this.props.ingredients).map(
+      (igKey) => {
+        return (
+          <li key={igKey}>
+            <span style={{ textTransform: "capitalize" }}>{igKey}</span>:{" "}
+            {this.props.ingredients[igKey]}
+          </li>
+        );
+      }
     );
-  });
-  return (
-    <React.Fragment>
-      <h3>Your Order</h3>
-      <p>A delicious burger with the following ingredients:</p>
-      <ul>{ingredientSummary}</ul>
-      <p>
-        <strong>Total price: {props.price.toFixed(2)}</strong>
-      </p>
-      <p>Continue to checkout?</p>
-      <Button buttonType="Danger" clicked={props.purchaseCancelled}>
-        CANCEL
-      </Button>
-      <Button buttonType="Success" clicked={props.purchaseContinued}>
-        CONTINUE
-      </Button>
-    </React.Fragment>
-  );
+    return (
+      <React.Fragment>
+        <h3>Your Order</h3>
+        <p>A delicious burger with the following ingredients:</p>
+        <ul>{ingredientSummary}</ul>
+        <p>
+          <strong>Total price: {this.props.price.toFixed(2)}</strong>
+        </p>
+        <p>Continue to checkout?</p>
+        <Button buttonType="Danger" clicked={this.props.purchaseCancelled}>
+          CANCEL
+        </Button>
+        <Button buttonType="Success" clicked={this.props.purchaseContinued}>
+          CONTINUE
+        </Button>
+      </React.Fragment>
+    );
+  }
 }
