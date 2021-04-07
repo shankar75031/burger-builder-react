a38ca5cc28e1e1a21b7faa94dfa3a15d69f8ac6f
Prabhashankar Kannapan <shankar75031@gmail.com>
2021-04-07 21:32:33 +0530

    created build control component and added styles

---
+++ src/components/Burger/BuildControls/BuildControls.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import classes from "./BuildControls.module.css";
+
+export default function BuildControls(props) {
+  return <div className={classes.BuildControls}></div>;
+}
