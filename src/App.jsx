bd80bb9e61cbc01b697d5a3e8c6aded0653eb9a8
Prabhashankar Kannapan <shankar75031@gmail.com>
2021-04-07 20:03:55 +0530

    cleanup final and initial setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      Test
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
