d107607ac400e2c272aab55094b7509e1de0c953
Prabhashankar Kannapan <shankar75031@gmail.com>
2021-04-11 00:30:47 +0530

    added navigation on clicking buttons in checkout

--- src/components/Order/CheckoutSummary/CheckoutSummary.jsx
+++ src/components/Order/CheckoutSummary/CheckoutSummary.jsx
@@ -9,10 +9,10 @@ export default function CheckoutSummary(props) {
       <h1>We hope it tastes well!</h1>
       <div style={{ width: "100%", margin: "auto" }}>
         <Burger ingredients={props.ingredients} />
-        <Button btnType="Danger" clicked>
+        <Button buttonType="Danger" clicked={props.onCheckoutCancelled}>
           CANCEL
         </Button>
-        <Button btnType="Success" clicked>
+        <Button buttonType="Success" clicked={props.checkoutContinued}>
           CONTINUE
         </Button>
       </div>
