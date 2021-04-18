fdae6268770de8475f9cfec301a075f367952aec
Prabhashankar Kannapan <shankar75031@gmail.com>
2021-04-19 03:50:04 +0530

    testing reducers

---
+++ src/store/reducers/auth.test.js
@@ -0,0 +1,35 @@
+import reducer from "./auth";
+import * as actionTypes from "../actions/actionTypes";
+
+describe("auth reducer", () => {
+  it("should return the initial state", () => {
+    expect(reducer(undefined, {})).toEqual({
+      token: null,
+      userId: null,
+      error: null,
+      loading: false,
+      authRedirect: "/",
+    });
+  });
+
+  it("should store the token upon login", () => {
+    expect(
+      reducer(
+        {
+          token: null,
+          userId: null,
+          error: null,
+          loading: false,
+          authRedirect: "/",
+        },
+        { type: actionTypes.AUTH_SUCCESS, idToken: "Hello", userId: "User" }
+      )
+    ).toEqual({
+      token: "Hello",
+      userId: "User",
+      error: null,
+      loading: false,
+      authRedirect: "/",
+    });
+  });
+});
