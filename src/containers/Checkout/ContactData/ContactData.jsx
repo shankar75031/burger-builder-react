6b8e9d62953e35fda53d708451e62fa83fc4bf06
Prabhashankar Kannapan <shankar75031@gmail.com>
2021-04-19 02:44:16 +0530

    added updateObject function in form input handling and refactoring

--- src/containers/Checkout/ContactData/ContactData.jsx
+++ src/containers/Checkout/ContactData/ContactData.jsx
@@ -7,6 +7,7 @@ import Spinner from "../../../components/UI/Spinner/Spinner";
 import withErrorHandler from "../../../hoc/withErrorHandler/withErrorHandler";
 import classes from "./ContactData.module.css";
 import * as actions from "../../../store/actions/index";
+import { updateObject } from "../../../shared/utility";
 class ContactData extends Component {
   state = {
     orderForm: {
@@ -135,17 +136,20 @@ class ContactData extends Component {
   };
 
   inputChangedHandler = (event, inputIdentifier) => {
-    const updatedOrderForm = {
-      ...this.state.orderForm,
-    };
-    const updatedFormElement = { ...updatedOrderForm[inputIdentifier] };
-    updatedFormElement.value = event.target.value;
-    updatedFormElement.valid = this.checkValidity(
-      updatedFormElement.value,
-      updatedFormElement.validation
+    const updatedFormElement = updateObject(
+      this.state.orderForm[inputIdentifier],
+      {
+        value: event.target.value,
+        valid: this.checkValidity(
+          event.target.value,
+          this.state.orderForm[inputIdentifier].validation
+        ),
+        touched: true,
+      }
     );
-    updatedFormElement.touched = true;
-    updatedOrderForm[inputIdentifier] = updatedFormElement;
+    const updatedOrderForm = updateObject(this.state.orderForm, {
+      [inputIdentifier]: updatedFormElement,
+    });
     console.log(updatedFormElement);
     let formIsValid = true;
     for (const inputIdentifier in updatedOrderForm) {
