be16ca0e17715e6839fb3864a29a2c03c201da39
Prabhashankar Kannapan <shankar75031@gmail.com>
2021-04-21 19:07:27 +0530

    auth saga completed

--- src/store/sagas/auth.js
+++ src/store/sagas/auth.js
@@ -13,7 +13,7 @@ export function* logoutSaga(action) {
 }
 
 export function* checkAuthTimeoutSaga(action) {
-  yield delay(action.expirationTime);
+  yield delay(action.expirationTime * 1000);
   yield put(actions.logout());
 }
 
@@ -39,5 +39,27 @@ export function* authUserSaga(action) {
     yield put(actions.checkAuthTimeout(res.data.expiresIn));
   } catch (err) {
     yield put(actions.authFail(err.response.data.error));
+  }
+}
+
+export function* authCheckStateSaga(action) {
+  const token = yield localStorage.getItem("token");
+  if (!token) {
+    yield put(actions.logout());
+  } else {
+    const expirationDate = yield new Date(
+      localStorage.getItem("expirationDate")
+    );
+    if (expirationDate <= new Date()) {
+      yield put(actions.logout());
+    } else {
+      const userId = yield localStorage.getItem("userId");
+      yield put(actions.authSuccess(token, userId));
+      yield put(
+        actions.checkAuthTimeout(
+          (expirationDate.getTime() - new Date().getTime()) / 1000
+        )
+      );
+    }
   }
 }
--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -1,4 +1,9 @@
-import { authUserSaga, checkAuthTimeoutSaga, logoutSaga } from "./auth";
+import {
+  authCheckStateSaga,
+  authUserSaga,
+  checkAuthTimeoutSaga,
+  logoutSaga,
+} from "./auth";
 import * as actionTypes from "../actions/actionTypes";
 import { takeEvery } from "@redux-saga/core/effects";
 
@@ -6,4 +11,5 @@ export function* watchAuth() {
   yield takeEvery(actionTypes.AUTH_INITIATE_LOGOUT, logoutSaga);
   yield takeEvery(actionTypes.AUTH_CHECK_TIMEOUT, checkAuthTimeoutSaga);
   yield takeEvery(actionTypes.AUTH_USER, authUserSaga);
+  yield takeEvery(actionTypes.AUTH_CHECK_STATE, authCheckStateSaga);
 }
