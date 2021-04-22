59baab02e378718d744d553067a6151ee2322c7c
Prabhashankar Kannapan <shankar75031@gmail.com>
2021-04-23 00:47:29 +0530

    changed orders and checkout to functional compoentns

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ const App = (props) => {
 
   let routes = (
     <Switch>
-      <Route path="/auth" render={() => <Auth />} />
+      <Route path="/auth" render={(props) => <Auth {...props} />} />
       <Route path="/" exact component={BurgerBuilder} />
       <Redirect to="/" />
     </Switch>
@@ -26,9 +26,9 @@ const App = (props) => {
   if (props.isAuthenticated) {
     routes = (
       <Switch>
-        <Route path="/orders" render={() => <Orders />} />
-        <Route path="/checkout" render={() => <Checkout />} />
-        <Route path="/auth" render={() => <Auth />} />
+        <Route path="/orders" render={(props) => <Orders {...props} />} />
+        <Route path="/checkout" render={(props) => <Checkout {...props} />} />
+        <Route path="/auth" render={(props) => <Auth {...props} />} />
         <Route path="/logout" component={Logout} />
         <Route path="/" exact component={BurgerBuilder} />
         <Redirect to="/" />
