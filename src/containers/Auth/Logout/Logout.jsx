631cb549cb70253cd10f9efe27cae3754dbb7a95
Prabhashankar Kannapan <shankar75031@gmail.com>
2021-04-23 01:13:25 +0530

    logout made functional

--- src/containers/Auth/Logout/Logout.jsx
+++ src/containers/Auth/Logout/Logout.jsx
@@ -1,16 +1,15 @@
-import React, { Component } from "react";
+import React, { Component, useEffect } from "react";
 import { connect } from "react-redux";
 import { Redirect } from "react-router";
 import * as actions from "../../../store/actions/index";
 
-class Logout extends Component {
-  componentDidMount() {
-    this.props.onLogout();
-  }
-  render() {
-    return <Redirect to="/" />;
-  }
-}
+const Logout = (props) => {
+  useEffect(() => {
+    props.onLogout();
+  }, []);
+
+  return <Redirect to="/" />;
+};
 
 const mapDispatchToProps = (dispatch) => {
   return {
